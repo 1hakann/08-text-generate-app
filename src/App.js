93f732ac5a2f41d85bcd58acf35073d6af7646d4
1hakann <hakan.cakmak@live.com>
2022-01-15 02:34:28 +0300

    lorem ipsum app has been completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import data from './data/data';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    if(count <= 0) {
+      amount = 0;
+    }
+    if(count > 5) {
+      amount = 5;
+    }
+    setText(data.slice(0, amount));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className='section-center'>
+      <h3>lorem ipsum app</h3>
+      <form className='lorem-form' onSubmit={handleSubmit}>
+        <label htmlFor='amount'>Sentence</label>
+        <input type="number" id='amount' name='amount' 
+        value={count} onChange={(e) => setCount(e.target.value)}></input>
+        <button type='submit' className='btn'>generate</button>
+      </form>
+      <article>
+        {
+          text.map((item, index) => {
+            return <p key={index}>{item}</p>
+          })
+        }
+      </article>
+    </section>
   );
 }
 
